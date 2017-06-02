1494a0588987fe5b4b4e571f797735d5409384ca
Caleb Johnson <joh16002@byui.edu>
2017-06-02 11:33:32 -0600

    test

--- web/input.php
+++ web/input.php
@@ -16,7 +16,7 @@ $name = $_POST['name'];
 $num = $_POST['num'];
 $collection_id = $_POST['collection_id'];
 $value = $_POST['value'];
-
+echo "test";
 $statement = $db->prepare("INSERT INTO card (name, num, collection_id, value) VALUES (:name, :num, :collection_id, :value)");
 $statement->bindValue(':name', $name);
 $statement->bindValue(':num', $num);
